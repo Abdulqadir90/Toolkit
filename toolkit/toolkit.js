1fd32c24cd0c1ef5536ce557d2416febe5e4a9c7
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-19 21:12:42 +0100

    row of stars in progress

--- toolkit/toolkit.js
+++ toolkit/toolkit.js
@@ -7,3 +7,20 @@ while(count <= 10) {
 
 // Exercise 2
 
+
+let stars = 4;
+let numberOfRows = 1;
+while(numberOfRows <= stars){
+    let count = 0;
+    let row = "";
+
+    while(count < stars){
+        row = row + "*";
+        count = count + 1;
+    }
+    console.log(row);
+    stars = stars + 1;
+
+}
+
+
