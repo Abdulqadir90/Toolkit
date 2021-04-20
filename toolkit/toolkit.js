7891dfe1bc7829d5aacf660d3a07135a28d7004a
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-20 19:31:51 +0100

    expected output arrays 4

--- toolkit/toolkit.js
+++ toolkit/toolkit.js
@@ -20,11 +20,27 @@
 
 // Exercise 3
 
-for (let i = 0; i < 16; i++) {
-    if( i % 2 == 0){
-        console.log(i + "even number")
-    } else {
-        console.log(i + "odd number")
-    }
+// for (let i = 0; i < 16; i++) {
+//     if( i % 2 == 0){
+//         console.log(i + "even number")
+//     } else {
+//         console.log(i + "odd number")
+//     }
+// }
+
+// Exercise 4
+
+const array1 = [1,0,2,3,4];
+const array2 = [3,5,6,7,8,13];
+
+let sums = [];
+
+for (let i = 0; i < array1.length; i++){
+    sums.push(array1[i] + array2[i])
+    
 }
+    sums.push(array2[array2.length-1])
+    console.log(sums)
+
+
 
