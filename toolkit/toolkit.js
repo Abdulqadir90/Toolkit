37c990ba672b7e7bf87d408cb672f129f87722f0
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-20 19:10:17 +0100

    row of stars done

--- toolkit/toolkit.js
+++ toolkit/toolkit.js
@@ -1,26 +1,25 @@
 // Exercise 1 
-let count = 1;
-while(count <= 10) {
-    console.log(count);
-    count = count +1;
-}
+// let count = 1;
+// while(count <= 10) {
+//     console.log(count);
+//     count = count +1;
+// }
 
 // Exercise 2
 
 
-let stars = 4;
-let numberOfRows = 1;
-while(numberOfRows <= stars){
-    let count = 0;
-    let row = "";
+// let stars = "";
+// let numberOfRows = 10;
+// let count = 1;
+// while(count <= numberOfRows){
+//     count = count + 1;
+//     stars = stars + "*";
+//     console.log(stars)
 
-    while(count < stars){
-        row = row + "*";
-        count = count + 1;
-    }
-    console.log(row);
-    stars = stars + 1;
+// }
 
-}
+// Exercise 3
+
+
 
 
