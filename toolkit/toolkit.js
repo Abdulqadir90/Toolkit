4019f38370dbfe30288f3b70fbcbb3c8e17a2ea7
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-20 19:42:06 +0100

    greater than /equal to 40

--- toolkit/toolkit.js
+++ toolkit/toolkit.js
@@ -30,17 +30,25 @@
 
 // Exercise 4
 
-const array1 = [1,0,2,3,4];
-const array2 = [3,5,6,7,8,13];
+// const array1 = [1,0,2,3,4];
+// const array2 = [3,5,6,7,8,13];
 
-let sums = [];
+// let sums = [];
 
-for (let i = 0; i < array1.length; i++){
-    sums.push(array1[i] + array2[i])
+// for (let i = 0; i < array1.length; i++){
+//     sums.push(array1[i] + array2[i])
     
+// }
+//     sums.push(array2[array2.length-1])
+//     console.log(sums)
+
+
+// Exercise 5
+
+const scores = [73, 35, 40, 68, 67, 91, 42, 48, 39, 55, 21, 95];
+
+for (let i = 0; i < scores.length; i++){
+    if(scores[i] >=40 ) {
+        console.log(scores[i])
+    }
 }
-    sums.push(array2[array2.length-1])
-    console.log(sums)
-
-
-
