e6e84ced3181935f020fb133b3af4b29d73e6e04
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-04-30 19:17:50 +0100

    check occurencies

--- toolkit/functions.js
+++ toolkit/functions.js
@@ -152,3 +152,20 @@
 // }
 // longestWord('Web Development Tutorial');
 
+// Write a JavaScript function that accepts two arguments, a string and a letter and the
+// function will count and return the number of occurrences of the specified letter within
+// the string.
+// Sample arguments : ’code your future’, ’u’
+// Expected output : 3
+
+// function check(str, letter) {
+//     let count = 0;
+//     for (let i=0; i<str.length; i++) {
+//         if (str[i] === letter) {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// check('hello', 'l');
